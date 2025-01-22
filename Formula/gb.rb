4fd9a33ad855f297cbff1978e8b2f19728c8bc03
Ryo Okamoto <ryo@aquahill.net>
2025-01-22 21:29:31 +0900

    upgraded gb to v0.1.3.

--- Formula/gb.rb
+++ Formula/gb.rb
@@ -1,26 +1,26 @@
 class Gb < Formula
   desc "A Tiny tool for displaying your git branches nicely."
   homepage "https://github.com/ryochin/gb"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/ryochin/gb/releases/download/v#{version}/gb-v#{version}-aarch64-apple-darwin"
-      sha256 "dbd59d2d8df7a24ee74e7215244f43510d0c89bb4b176b980d0f11085dedd746"
+      sha256 "5d3c63457b3d3b460b816e9bddc3872992b03ecabb1246f3644b96f5fe106163"
     else
       url "https://github.com/ryochin/gb/releases/download/v#{version}/gb-v#{version}-x86_64-apple-darwin"
-      sha256 "b6a456b3884d2de6332fedacde8c4561c0a402594dd12d174ec9163c3c85fa9e"
+      sha256 "91632a5b6e02c92151d730397cd1f5b5109576812e006f61aab2c76a3b0204dc"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/ryochin/gb/releases/download/v#{version}/gb-v#{version}-aarch64-unknown-linux-gnu"
-      sha256 "3199127ea33a297c4ad25e504039da53ee195e8d118af96a653414763987713c"
+      sha256 "2401d9a3c07dcbb06e9fbd4498a8df1395d8d90861b5a1a976d487e38243da6a"
     else
       url "https://github.com/ryochin/gb/releases/download/v#{version}/gb-v#{version}-x86_64-unknown-linux-gnu"
-      sha256 "40d78990bbef8f283133ef1ef15e630b8056ce7399cf0eb218d7cdcd7ffd8010"
+      sha256 "1411842890a39c73b9ffd0f523ad1f3524e5b9e0f320f7b291125c0122793b98"
     end
   end
 
